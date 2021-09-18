c0405e8b7cad46a7053a0e7cdddf916cb7511c1b
xulihang <xulihanghai@163.com>
2021-09-18 10:18:18 +0800

    add https support

--- websocket-server/src/com/dynamsoft/server/WebSocketServer.java
+++ websocket-server/src/com/dynamsoft/server/WebSocketServer.java
@@ -32,6 +32,31 @@ public class WebSocketServer {
 		handlerList.setHandlers(new Handler[]{new WSHandler(),context});
 		server.setHandler(handlerList);
 		
+		HttpConfiguration http_config = new HttpConfiguration();
+		http_config.setSecureScheme("https");
+		http_config.setSecurePort(8443);
+
+		HttpConfiguration https_config = new HttpConfiguration(http_config);
+		https_config.addCustomizer(new SecureRequestCustomizer());
+
+		SslContextFactory sslContextFactory = new SslContextFactory();
+		sslContextFactory.setKeyStorePath("keystore.jks");
+		sslContextFactory.setKeyStorePassword("password1");
+
+		ServerConnector wsConnector = new ServerConnector(server);
+		wsConnector.setHost(null);
+		wsConnector.setPort(8080);
+		server.addConnector(wsConnector);
+
+		ServerConnector wssConnector = new ServerConnector(server,
+				new SslConnectionFactory(sslContextFactory,
+					HttpVersion.HTTP_1_1.asString()),
+				new HttpConnectionFactory(https_config)); // THIS WAS MISSING
+
+		wssConnector.setHost(null);
+		wssConnector.setPort(8443);
+		server.addConnector(wssConnector);
+		
         server.setStopTimeout(0);
         server.start();
         server.join();
