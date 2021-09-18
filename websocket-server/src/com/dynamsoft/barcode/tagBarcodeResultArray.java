59009dad3810e02bcbdf57beef763914df505566
xulihang <xulihanghai@163.com>
2021-09-18 10:14:46 +0800

    serve static files in www folder and remove jbarcode

--- websocket-server/src/com/dynamsoft/barcode/tagBarcodeResultArray.java
+++
@@ -1,6 +0,0 @@
-package com.dynamsoft.barcode;
-
-public class tagBarcodeResultArray {
-	public int iBarcodeCount;
-	public tagBarcodeResult[] ppBarcodes;
-}
--- websocket-server/src/com/dynamsoft/server/WebSocketServer.java
+++ websocket-server/src/com/dynamsoft/server/WebSocketServer.java
@@ -1,12 +1,37 @@
 package com.dynamsoft.server;
 
+import java.io.File;
+
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 
 public class WebSocketServer {
 	
     public static void main(String[] args) throws Exception {
         Server server = new Server(88);
-        server.setHandler(new WSHandler());
+        File f = new File("www");
+	 	  
+		ServletContextHandler context = new ServletContextHandler(
+					ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		context.setBaseResource(Resource.newResource(f.toURI()));
+		context.setWelcomeFiles(new String[] { "index.html" });
+
+		ServletHolder holderPwd = new ServletHolder("default",
+					DefaultServlet.class);
+		holderPwd.setInitParameter("dirAllowed", "true");
+		context.addServlet(holderPwd, "/");
+		
+		
+		HandlerCollection handlerList = new HandlerCollection();
+		handlerList.setHandlers(new Handler[]{new WSHandler(),context});
+		server.setHandler(handlerList);
+		
         server.setStopTimeout(0);
         server.start();
         server.join();
